4698721ed89b2fa22b1be9ad9b3d0159f8ffaf51
Austin <aturn9880@gmail.com>
2021-06-13 16:10:08 -0400

    file upload

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const characterList = document.getElementById('character-list');
+    
+    
+    fetch(`https://rickandmortyapi.com/api/character/`)
+    .then(resp => resp.json())
+    .then(characters => showCharacters(characters.results));
+    // .then(obj => console.log(obj.results));
+     
+    const showCharacters = characters => {
+        characters.forEach(character => {
+            const characterElement = document.createElement('li');
+            characterElement.innerHTML = `${character.name}`;
+            characterList.append(characterElement);
+        });
+    }
+    
+    
+})
